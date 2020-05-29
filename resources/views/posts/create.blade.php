3a157e5e46620d2f0f608cc3b19d75ed87cd63c0
Engy Ashraf <egy2962@gmail.com>
2020-05-29 02:41:29 -0700

    Modify Views

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <h1>Create Post</h1>
-    {!! Form::open(['action' => 'PostController@store', 'method' => 'POST']) !!}
+    {!! Form::open(['action' => 'PostController@store', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
         <div class = "form-group">
             {{Form::label('title', 'Title')}}
             {{Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Title'])}}
@@ -11,8 +11,28 @@
             {{Form::label('body', 'Body')}}
             {{Form::textarea('body', '', ['class' => 'form-control', 'placeholder' => 'Body Text'])}}
         </div>
-        
+        <div class="form-group">
+             {{Form::select('tags', array(
+                'Computer Science' => 'Computer Science',
+                'Ethical Hacking' => 'Ethical Hacking',
+                'information technology' => 'information technology',
+                'data science' => 'data science',
+                'artificial intillegince' => 'artificial intillegince',
+                'machine learning' => 'machine learning',
+                'deep learning' => 'deep learning',
+                'web development' => 'web development',
+                'mobile development' => 'mobile development',
+                'java' => 'java',
+                'python' => 'python',
+                'c++' => 'c++',
+            ))}}
+        </div>
+        <div class="form-group">
+            {{Form::file('cover_image')}}
+        </div>
+
+
         {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
     {!! Form::close() !!}
-    
+
 @endsection
