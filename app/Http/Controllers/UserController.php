cee6d40a9a1160df5465f78db4da5a79c91a4c75
Ahmed Radi <ahmedradi743@gmail.com>
2020-05-30 18:32:55 +0200

    Add Edit to User Controller & Update in database

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,4 +14,55 @@ class UserController extends Controller
         return view('users.profile')->withUser($user);
     }
 
+    public function edit(){
+        if(Auth::user()){
+            $user = User::find(Auth::user()->id);
+            if($user){
+                return view('users.edit')->withUser($user);
+
+            }else{
+                return redirect()->back();
+            }
+
+        }else{
+            return redirect()->back();
+
+        }
+    }
+    public function update(Request $request){
+        $user = User::find(Auth::user()->id);
+
+        if($user){
+            $validate = null;
+            if(Auth::user()->email === $request['email']){
+                $validate = $request->validate([
+                    'name'=>'required|min:2',
+                    'email'=>'required|email'
+                ]);
+            }else{
+                $validate = $request->validate([
+                    'name'=>'required|min:2',
+                    'email'=>'required|email|unique:users'
+                ]);
+            }
+
+
+            if($validate){
+                $user->name = $request['name'];
+                $user->email = $request['email'];
+                $user->save();
+                $request->session()->flash('success', 'Your details have been updated!');
+                return redirect()->back();
+            }else{
+                return redirect()->back();
+            }
+
+        }else{
+            return redirect()->back();
+        }
+
+
+    }
+
+
 }
